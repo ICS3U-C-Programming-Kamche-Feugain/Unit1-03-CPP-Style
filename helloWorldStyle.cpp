c8af591645c6f47a94cb499dd178575d98657e75
Kamche Feugain Sofo <kamche.feugain-sofo@stu.ocsb.ca>
2026-09-15 13:05:46 +0000

    First commit of helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2026 Kamche All rights reserved.
+// .
+// Created by: Your Kamche
+// Date: Sep 15, 2026
+// This program displays a greeting with proper style.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Kamche" << std::endl;
+    std::cout << "This class is so much fun" << std::endl;
+
+    return 0;
+}
